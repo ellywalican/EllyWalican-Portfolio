cc25cfee740038773818c8579d3dc2cd51401595
Elly Walican <ellyboywalican@gmail.com>
2025-07-20 00:02:46 +0800

    Fix: Redirect unknown paths to Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -15,6 +15,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
+
+        {/* Redirect unknown paths to Home */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
   );
