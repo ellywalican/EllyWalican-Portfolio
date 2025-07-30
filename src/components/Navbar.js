b44126339dd8789d3912007d20ca351f1355474d
Elly Walican <ellyboywalican@gmail.com>
2025-07-30 09:32:37 +0800

    Update Navbar component with logo and navigation links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@
     return (
       <nav className="navbar">
         <div className="logo-title">
-        <img src="https://scontent.fmnl7-1.fna.fbcdn.net/v/t39.30808-1/506475483_1915681675948655_1051936473401037800_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=109&ccb=1-7&_nc_sid=1d2534&_nc_eui2=AeHTxh5sVy1b8j-zTavPMwDcCY188kw5-ysJjXzyTDn7K1ubKitxCbEXtAqsejvs6T8hg4GNA9Q6EA7g92f5RwS1&_nc_ohc=6IEh0becCq4Q7kNvwHSd69i&_nc_oc=AdkKeBPICpy2nmDThY-HewLOH6XqIcOB7fVExkqUEvWC7YKt6zJUrK0FO-MMUKQYuqo&_nc_zt=24&_nc_ht=scontent.fmnl7-1.fna&_nc_gid=WhT3zd7gsWGih8gSZJx29w&oh=00_AfQa3RNDxxfmHKCjpFw2ay34XDksxJFlCqeilQrXbmHJ3g&oe=68815DB7" alt="Logo" className="logo" />
+        <img src="https://scontent.fmnl13-4.fna.fbcdn.net/v/t39.30808-1/524595065_1948156789367810_8538665807610987039_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=107&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeF17WzyYpe3c5rq23gyflv7ittEisPpf0KK20SKw-l_Qsrf3gFZiXWN5pEV_lzyZgCBKsUilfg9bib6Zcb1rZoq&_nc_ohc=daJSNyejVLgQ7kNvwHyZXnl&_nc_oc=AdlOD_G1k7RJigxpFz8U_Mj3LXDohs8I_3Snh0L8kVQdCQTuBIbkmIkqpquZlJn4yOk&_nc_zt=24&_nc_ht=scontent.fmnl13-4.fna&_nc_gid=OtcqmYoyfhonamrqDtq7mg&oh=00_AfSh6lsbxHJCBTWZP_iNpQ5S5HMlpahykLe3TpiZFScljQ&oe=688F4606" alt="Logo" className="logo" />
         <h2>Elly Walican</h2>
       </div>
         <ul>
